b34665a2f96842936ba3a8a309775056a4b1cb7e
KhalidAbrar2000 <khalid.abrar.labib@g.bracu.ac.bd>
2026-04-18 21:42:08 +0600

    Initial MERN MVC setup

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const connectDB = require('./config/db');
+
+// Load environment variables
+dotenv.config();
+
+// Connect to Database
+connectDB();
+
+const app = express();
+
+// Middleware so Express understands JSON data
+app.use(express.json());
+app.use(cors());
+
+// A simple test route
+app.get('/', (req, res) => {
+  res.send('Krishi-Connect API is running!');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
